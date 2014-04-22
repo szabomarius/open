da18454bdd2db856d0b68d892c8d2db9e4d706f1
Szabo Marius <szabomarius@gmail.com>
2014-04-22 14:14:35 +0300

    added some translations for the ladies

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,19 @@
 // custom js
+$(document).ready(function() {
+
+// Make buttons active or not
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+var nav_buttons = $('.main_nav a');
+var nav_circles = $('.nav_btns li');
+
+nav_buttons.click(function() {
+	nav_buttons.removeClass("active");
+	$(this).addClass("active");
+})
+nav_circles.click(function() {
+	nav_circles.removeClass("active");
+	$(this).addClass("active");
+})
+
+}); //end of document.ready()
